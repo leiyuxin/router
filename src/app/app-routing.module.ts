ce0e05014d50c34f9c813652d64fc31f027368ad
leiyuxin <617705033@qq.com>
2017-09-07 00:31:29 +0800

    redirectTo,full 或者prefix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,8 +5,12 @@ import { StockComponent } from './stock/stock.component';
 import { Code404Component } from './code404/code404.component';
 // 不要使用/ 开头
 const routes: Routes = [
+  { // 只有当path 为'' 才跳转是full的已有,prefix 前缀 指的是当path vlaue 以xx 开头就可以
+    path: '', redirectTo: '/home', pathMatch: 'full'
+
+  },
   {
-    path: '', component: HomeComponent
+    path: 'home', component: HomeComponent
 
   },
   {// 1.使其能够携带参数
